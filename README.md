d4b784bf42c1a51bfabccd137781b93f139803d7
GaganAgarwal77 <Gagan.Agarwal@iiitb.ac.in>
2022-02-20 02:51:32 +0530

    Ready for submission

---
+++ README.md
@@ -0,0 +1,7 @@
+## Instructions to use this app
+
+- To host the app locally, you'll need either the VSCode Live Server extension or use python to start a simple HTTP server , using the command  `python3 -m http.server` or `python -m SimpleHTTPServer`.
+
+- Once the app is live on a local address, you can view the application in mode-0 by default, with a reference area and drawing area.
+
+- The app can then be used as specified in the assignment requirements.
--- index.js
+++ index.js
@@ -4,171 +4,163 @@ import {fragmentShaderSrc} from './shaders/fragment.js';
 import { Square } from './lib/square.js';
 import { Parallelogram } from './lib/parallelogram.js';
 
-import { vec3, mat4 } from 'https://cdn.skypack.dev/gl-matrix';
+import { vec3 } from 'https://cdn.skypack.dev/gl-matrix';
 
 
 function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
 }
-function getRandomPosition() {
-    return getRandomArbitrary(-1,1)
-}
-function getRandomColour() {
-    return getRandomArbitrary(0,1)
-}
 
 
 const renderer = new WebGLRenderer();
-renderer.setSize( 1400, 800 );
-
-let gl = renderer.glContext()
+renderer.setSize( 1400, 800 ); //creating our canvas
 document.body.appendChild( renderer.domElement );
 
 
+// variables for implementation
 let currentKeyDown  = "t"
 let currPrimitiveIndex  = 0
 let currMode = 0
 let prevMode = 0
+
+let gl = renderer.glContext()
 const shader = new Shader(gl, vertexShaderSrc, fragmentShaderSrc);
 shader.use()
+
 const width = gl.canvas.width;
 const height = gl.canvas.height;
 
+const sceneL = new Scene(); // On the Left : Reference Scene
+const sceneR = new Scene(); // On the Right: Drawing Scene
 
-const sceneL = new Scene();
-const sceneR = new Scene();
-
+// Div for seeing current mode, key clicked, primitive selected
 let div = document.createElement('div')
-div.setAttribute("id", "div1");
+div.setAttribute("id", "div1"); 
 div.innerText = `Current Mode = M${currMode} \n Current Key Down = ${currentKeyDown} \n Current Primitive Index = ${currPrimitiveIndex}`
-
-document.body.appendChild( div);
+document.body.appendChild(div);
 
 function setupReferenceScene(){
-    const t0 = new Triangle(0,0,[1,153/255,0,1]); // orange
-    const t1 = new Triangle(0,0,[74/255,134/255,232/255,1]); // blue
-    const t2 = new Triangle(0,0,[1, 1, 0,1]); // yellow
-    const t3 = new Triangle(0,0,[0,1,0,1]); // green
-    const t4 = new Triangle(0,0,[0, 1, 1,1]); // light blue
-    const s1 = new Square(0,0,[1,0,0,1]); // red
-    const p1 = new Parallelogram(0,0,[1,0,1,1]); // pink
+
+    // Creating shapes with  colors
+    const shapes = [0,0,0,0,0,0,0]
+    shapes[0] = new Triangle(0, 0, [1, 153/255, 0, 1]); 
+    shapes[1] = new Triangle(0, 0, [74/255, 134/255, 232/255, 1]); 
+    shapes[2] = new Triangle(0, 0, [1,  1,  0, 1]);
+    shapes[3] = new Triangle(0, 0, [0, 1, 0, 1]); 
+    shapes[4] = new Triangle(0, 0, [0,  1,  1, 1]); 
+    shapes[5] = new Square(0, 0, [1, 0, 0, 1]); 
+    shapes[6] = new Parallelogram(0, 0, [1, 0, 1, 1]);
 
 
     const rotationAxis = vec3.fromValues(0, 0, 1)
 
-    t0.transform.setTranslate(vec3.fromValues(-0.24 + 0.25, 0.32, 0));
-    t0.transform.setRotate(rotationAxis,0.78);
+    // Translating, rotating and scaling individual shapes to form the initial configuration
+
+    shapes[0].transform.setTranslate(vec3.fromValues(-0.24 + 0.25, 0.32, 0));
+    shapes[0].transform.setRotate(rotationAxis,0.78);
+    shapes[0].transform.setScale([2, 2,1]);
 
 
-    t1.transform.setTranslate(vec3.fromValues(0.04 + 0.25, 0.04, 0));
-    t1.transform.setRotate(rotationAxis,-0.78);
+    shapes[1].transform.setTranslate(vec3.fromValues(0.04 + 0.25, 0.04, 0));
+    shapes[1].transform.setRotate(rotationAxis,-0.78);
+    shapes[1].transform.setScale([2, 2,1]);
 
 
-    t2.transform.setTranslate(vec3.fromValues(0.05 + 0.25, -0.38, 0));
-    t2.transform.setRotate(rotationAxis,-2.34);
+    shapes[2].transform.setTranslate(vec3.fromValues(0.05 + 0.25, -0.38, 0));
+    shapes[2].transform.setRotate(rotationAxis,-2.34);
+    shapes[2].transform.setScale([1, 1,1]);
 
 
-    t3.transform.setTranslate(vec3.fromValues(-0.66 + 0.25, -0.38, 0));
-    t3.transform.setRotate(rotationAxis,0);
+    shapes[3].transform.setTranslate(vec3.fromValues(-0.66 + 0.25, -0.38, 0));
+    shapes[3].transform.setRotate(rotationAxis,0);
+    shapes[3].transform.setScale([1.5, 1.5,1]);
 
 
-    t4.transform.setTranslate(vec3.fromValues(-0.38 + 0.25, 0.04, 0));
-    t4.transform.setRotate(rotationAxis,2.36);
-
-    t0.transform.setScale([2, 2,1]);
-    t1.transform.setScale([2, 2,1]);
-    t2.transform.setScale([1, 1,1]);
-    t3.transform.setScale([1.5, 1.5,1]);
-    t4.transform.setScale([1, 1,1]);
-
-    s1.transform.setTranslate([-0.23 +0.25, -0.24, 0]);
-    s1.transform.setRotate(rotationAxis,-0.78);
-    s1.transform.setScale([1, 1,1]);
+    shapes[4].transform.setTranslate(vec3.fromValues(-0.38 + 0.25, 0.04, 0));
+    shapes[4].transform.setRotate(rotationAxis,2.36);
+    shapes[4].transform.setScale([1, 1,1]);
 
 
-    p1.transform.setTranslate([-0.67 +0.25, 0.19, 0]);
-    p1.transform.setRotate(rotationAxis,-0.78);
-    p1.transform.setScale([1, 1,1]);
+    shapes[5].transform.setTranslate([-0.23 +0.25, -0.24, 0]);
+    shapes[5].transform.setRotate(rotationAxis,-0.78);
+    shapes[5].transform.setScale([1, 1,1]);
 
-    sceneL.add(t0);
-    sceneL.add(t1);
-    sceneL.add(t2);
-    sceneL.add(t3);
-    sceneL.add(t4);
-    sceneL.add(s1);
-    sceneL.add(p1);
+
+    shapes[6].transform.setTranslate([-0.67 +0.25, 0.19, 0]);
+    shapes[6].transform.setRotate(rotationAxis,-0.78);
+    shapes[6].transform.setScale([1, 1,1]);
+
+    sceneL.primitives = [...shapes]
 }
 function setupDrawingScene(){
-    const t0 = new Triangle(0,0,[1,153/255,0,1]); // orange
-    const t1 = new Triangle(0,0,[74/255,134/255,232/255,1]); // blue
-    const t2 = new Triangle(0,0,[1, 1, 0,1]); // yellow
-    const t3 = new Triangle(0,0,[0,1,0,1]); // green
-    const t4 = new Triangle(0,0,[0, 1, 1,1]); // light blue
-    const s1 = new Square(0,0,[1,0,0,1]); // red
-    const p1 = new Parallelogram(0,0,[1,0,1,1]); // pink
+
+    // Creating shapes with  colors
+    const shapes = [0,0,0,0,0,0,0]
+    shapes[0] = new Triangle(0, 0, [1, 153/255, 0, 1]); 
+    shapes[1] = new Triangle(0, 0, [74/255, 134/255, 232/255, 1]); 
+    shapes[2] = new Triangle(0, 0, [1,  1,  0, 1]);
+    shapes[3] = new Triangle(0, 0, [0, 1, 0, 1]); 
+    shapes[4] = new Triangle(0, 0, [0,  1,  1, 1]); 
+    shapes[5] = new Square(0, 0, [1, 0, 0, 1]); 
+    shapes[6] = new Parallelogram(0, 0, [1, 0, 1, 1]);
 
 
     const rotationAxis = vec3.fromValues(0, 0, 1)
 
-    t0.transform.setTranslate(vec3.fromValues(getRandomArbitrary(-0.5,0.5),getRandomArbitrary(-0.5,0.5), 0));
-    t0.transform.setRotate(rotationAxis,0.78);
+    // Translating, rotating and scaling individual shapes to form a randomized configuration
+
+    shapes[0].transform.setTranslate(vec3.fromValues(getRandomArbitrary(-0.5,0.5),getRandomArbitrary(-0.5,0.5), 0));
+    shapes[0].transform.setRotate(rotationAxis,0.78);
+    shapes[0].transform.setScale([2, 2,1]);
 
 
-    t1.transform.setTranslate(vec3.fromValues(getRandomArbitrary(-0.5,0.5), getRandomArbitrary(-0.5,0.5), 0));
-    t1.transform.setRotate(rotationAxis,-0.78);
+    shapes[1].transform.setTranslate(vec3.fromValues(getRandomArbitrary(-0.5,0.5), getRandomArbitrary(-0.5,0.5), 0));
+    shapes[1].transform.setRotate(rotationAxis,-0.78);
+    shapes[1].transform.setScale([2, 2,1]);
 
 
-    t2.transform.setTranslate(vec3.fromValues(getRandomArbitrary(-0.5,0.5), getRandomArbitrary(-0.5,0.5), 0));
-    t2.transform.setRotate(rotationAxis,-2.34);
+    shapes[2].transform.setTranslate(vec3.fromValues(getRandomArbitrary(-0.5,0.5), getRandomArbitrary(-0.5,0.5), 0));
+    shapes[2].transform.setRotate(rotationAxis,-2.34);
+    shapes[2].transform.setScale([1, 1,1]);
 
 
-    t3.transform.setTranslate(vec3.fromValues(getRandomArbitrary(-0.5,0.5), getRandomArbitrary(-0.5,0.5), 0));
-    t3.transform.setRotate(rotationAxis,0);
+    shapes[3].transform.setTranslate(vec3.fromValues(getRandomArbitrary(-0.5,0.5), getRandomArbitrary(-0.5,0.5), 0));
+    shapes[3].transform.setRotate(rotationAxis,0);
+    shapes[3].transform.setScale([1.5, 1.5,1]);
 
 
-    t4.transform.setTranslate(vec3.fromValues(getRandomArbitrary(-0.5,0.5), getRandomArbitrary(-0.5,0.5), 0));
-    t4.transform.setRotate(rotationAxis,2.36);
-
-    t0.transform.setScale([2, 2,1]);
-    t1.transform.setScale([2, 2,1]);
-    t2.transform.setScale([1, 1,1]);
-    t3.transform.setScale([1.5, 1.5,1]);
-    t4.transform.setScale([1, 1,1]);
-
-    s1.transform.setTranslate([getRandomArbitrary(-0.5,0.5), getRandomArbitrary(-0.5,0.5), 0]);
-    s1.transform.setRotate(rotationAxis,-0.78);
-    s1.transform.setScale([1, 1,1]);    
+    shapes[4].transform.setTranslate(vec3.fromValues(getRandomArbitrary(-0.5,0.5), getRandomArbitrary(-0.5,0.5), 0));
+    shapes[4].transform.setRotate(rotationAxis,2.36);
+    shapes[4].transform.setScale([1, 1,1]);
 
 
-    p1.transform.setTranslate([getRandomArbitrary(-0.5,0.5), getRandomArbitrary(-0.5,0.5), 0]);
-    p1.transform.setRotate(rotationAxis,-0.78);
-    p1.transform.setScale([1, 1,1]);
+    shapes[5].transform.setTranslate([getRandomArbitrary(-0.5,0.5), getRandomArbitrary(-0.5,0.5), 0]);
+    shapes[5].transform.setRotate(rotationAxis,-0.78);
+    shapes[5].transform.setScale([1, 1,1]);    
 
-    sceneR.add(t0);
-    sceneR.add(t1);
-    sceneR.add(t2);
-    sceneR.add(t3);
-    sceneR.add(t4);
-    sceneR.add(s1);
-    sceneR.add(p1);
+
+    shapes[6].transform.setTranslate([getRandomArbitrary(-0.5,0.5), getRandomArbitrary(-0.5,0.5), 0]);
+    shapes[6].transform.setRotate(rotationAxis,-0.78);
+    shapes[6].transform.setScale([1, 1,1]);
+
+
+    sceneR.primitives = [...shapes]
 }
 
 
 
-const pointInCanvas = (x, y) => (
+const pointInCanvas = (x, y) => ( //Checking if point lies in our canvas 
     (x > -1 && x < 1) && (y > -1 && y < 1)
   )
   
-function getCurrentPrimitive(event) {
+function getCurrentPrimitive(event) { // Get primitive closest to mouse click position
     let posX = event.offsetX 
     let posY = event.offsetY
-    console.log(posX,posY)
     let closestPrimitive = 0
     let distance = 4
     let xy = renderer.mouseToClipCoord(posX-width/2,posY,[width/2,height])
     let x = xy[0]
     let y = xy[1]
-    console.log(x,y)
 
     if(posX > width/2 && posX < width && posY < height){
         for (let index = 0; index < sceneR.primitives.length; index++) {
@@ -177,9 +169,6 @@ function getCurrentPrimitive(event) {
             let cX = centroidXY[0]
             let cY = centroidXY[1]
             let currDistance =  Math.sqrt((x-cX)*(x-cX) + (y-cY)*(y-cY))
-            console.log("Click",x.toFixed(2),y.toFixed(2))
-            console.log("Centroid",cX.toFixed(2),cY.toFixed(2))
-            console.log("Distance",currDistance.toFixed(2))
             if(currDistance <= distance){
                 distance = currDistance
                 closestPrimitive = index
@@ -191,60 +180,52 @@ function getCurrentPrimitive(event) {
 
 
 function renderLoop(){
-    //left
+    // Reference Frame
     {
-    const drawX = 0;
-    const drawY = 0;
-    const drawWidth = width / 2;
-    const drawHeight = height;
-
-    gl.clearColor(0.9,0.9,0.9,1);
-    renderer.render(sceneL, shader,drawX, drawY, drawWidth, drawHeight,[0,1,0,1]) 
+    renderer.render(sceneL, shader,0, 0, width/2, height,[0,1,0,1]) 
     }
 
 
   
-    //right
+    // Drawing Frame
     {
-    const drawX = width / 2;
-    const drawY = 0;
-    const drawWidth = width / 2;
-    const drawHeight = height;
+    renderer.render(sceneR, shader,width/2, 0, width/2, height,[1,0,0,1]) 
 
-    gl.clearColor(0.9,0.9,0.9,1);
-    renderer.render(sceneR, shader,drawX, drawY, drawWidth, drawHeight,[1,0,0,1]) 
-    if(currentKeyDown === "m"){
+    if(currentKeyDown === "m") { // Mode switch
         currMode = (currMode +  1) % 4
-        console.log(currMode)
         let div = document.getElementById("div1")
-        console.log(div)
         div.innerText = `Current Mode = M${currMode} \n Current Key Down = ${currentKeyDown} \n Current Primitive Index = ${currPrimitiveIndex}`
         currentKeyDown = "t"
     }
-    if(prevMode === 3 && currMode === 0 ){
+
+    if(prevMode === 3 && currMode === 0 ) { // Going from mode-3 (empty canvas) to mode-0 (randomized canvas)
         setupDrawingScene()
         prevMode = 0
     }
-    if(currMode === 1){
+
+    if(currMode === 1) {
         let currPrimitive = sceneR.primitives[currPrimitiveIndex]
         let cXY = currPrimitive.getCentroid()
         let cX = cXY[0]
         let cY = cXY[1]
+
+        let XY = currPrimitive.transform.getTranslate()
+
+        //Checking for key pressed and making sure centroid is within the canvas, and translating/rotating/scaling accordingly
         if(currentKeyDown==="ArrowRight" && pointInCanvas(cX+0.02,cY)){
-            currPrimitive.transform.setTranslate(vec3.fromValues(currPrimitive.transform.getTranslate()[0] + 0.02, currPrimitive.transform.getTranslate()[1], 0));
+            currPrimitive.transform.setTranslate(vec3.fromValues(XY[0] + 0.02, XY[1], 0));
             currentKeyDown = "t"
-            
         }
         if(currentKeyDown==="ArrowLeft" && pointInCanvas(cX-0.02,cY)){
-            currPrimitive.transform.setTranslate(vec3.fromValues(currPrimitive.transform.getTranslate()[0] - 0.02, currPrimitive.transform.getTranslate()[1], 0));
+            currPrimitive.transform.setTranslate(vec3.fromValues(XY[0] - 0.02, XY[1], 0));
             currentKeyDown = "t"
         }
         if(currentKeyDown==="ArrowUp" && pointInCanvas(cX,cY+0.02)){
-            currPrimitive.transform.setTranslate(vec3.fromValues(currPrimitive.transform.getTranslate()[0] , currPrimitive.transform.getTranslate()[1] + 0.02, 0));
+            currPrimitive.transform.setTranslate(vec3.fromValues(XY[0] , XY[1] + 0.02, 0));
             currentKeyDown = "t"
         }
         if(currentKeyDown==="ArrowDown" && pointInCanvas(cX,cY-0.02)){
-            currPrimitive.transform.setTranslate(vec3.fromValues(currPrimitive.transform.getTranslate()[0] , currPrimitive.transform.getTranslate()[1] - 0.02, 0));
+            currPrimitive.transform.setTranslate(vec3.fromValues(XY[0] , XY[1] - 0.02, 0));
             currentKeyDown = "t"
         }
         if(currentKeyDown==="("){
@@ -256,32 +237,27 @@ function renderLoop(){
             currentKeyDown = "t"
         }
         if(currentKeyDown==="+"){
-            currPrimitive.transform.setScale(vec3.fromValues(currPrimitive.transform.getScale()[0] * 1.05, currPrimitive.transform.getScale()[0] * 1.05, 1));
+            currPrimitive.transform.setScale(vec3.fromValues(currPrimitive.transform.getScale()[0] * 1.05, currPrimitive.transform.getScale()[1] * 1.05, 1));
             currentKeyDown = "t"
         }
         if(currentKeyDown==="-"){
-            currPrimitive.transform.setScale(vec3.fromValues(currPrimitive.transform.getScale()[0] / 1.05, currPrimitive.transform.getScale()[0] / 1.05, 1));
+            currPrimitive.transform.setScale(vec3.fromValues(currPrimitive.transform.getScale()[0] / 1.05, currPrimitive.transform.getScale()[1] / 1.05, 1));
             currentKeyDown = "t"
         }
-        if(currentKeyDown==="x"){   
-            sceneR.primitives.pop()
-            currentKeyDown = "t"
-        }
-
         prevMode = 1
     }
     if(currMode === 2){
         let sceneCxy= sceneR.centroid()
         let cX = sceneCxy[0]
         let cY = sceneCxy[1]
-        console.log(cX,cY)
+        
+        // Checking for key pressed and making sure that centroid of the scene is within the canvas, and translating/rotating/scaling accordingly
         if(currentKeyDown==="ArrowRight" && pointInCanvas(cX+0.02,cY)){
             for (let index = 0; index < sceneR.primitives.length; index++) {
                 const primitive = sceneR.primitives[index];
                 primitive.transform.setTranslate(vec3.fromValues(primitive.transform.getTranslate()[0] + 0.02, primitive.transform.getTranslate()[1], 0));
             }
             currentKeyDown = "t"
-            
         }
         if(currentKeyDown==="ArrowLeft" && pointInCanvas(cX-0.02,cY)){
             for (let index = 0; index < sceneR.primitives.length; index++) {
@@ -341,25 +317,30 @@ function renderLoop(){
     }
     if(currMode === 3){
 
-        sceneR.primitives = []
+        sceneR.primitives = [] //Emptying the canvas in mode-3
         prevMode = 3
     }
     }
 }
+
+
+
 setupReferenceScene()
 setupDrawingScene()
-renderer.setAnimationLoop( renderLoop );
+
+renderer.setAnimationLoop( renderLoop ); // Starting the render loop
 
 
-document.addEventListener("click", function(event){
+
+//Event listeners for mouse click and key press
+document.addEventListener("click", function(event){ 
     getCurrentPrimitive(event)
     let div = document.getElementById("div1")
-    console.log(div)
     div.innerText = `Current Mode = M${currMode} \n Current Key Down = ${currentKeyDown} \n Current Primitive Index = ${currPrimitiveIndex}`
 });
-document.addEventListener("keydown", function(event) { console.log(event.key); currentKeyDown = event.key;
+document.addEventListener("keydown", function(event) { 
+    currentKeyDown = event.key;
     let div = document.getElementById("div1")
-    console.log(div)
     div.innerText = `Current Mode = M${currMode} \n Current Key Down = ${currentKeyDown} \n Current Primitive Index = ${currPrimitiveIndex}`
 });
 
